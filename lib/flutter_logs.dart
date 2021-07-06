2ee3e939bde71182b785313cdc4215b744dc0a26
gmendozah <geovanimendoza.h@gmail.com>
2021-07-05 23:24:08 -0400

    style: add comments for both methods
    deleted unnecessary comments

--- lib/flutter_logs.dart
+++ lib/flutter_logs.dart
@@ -1,20 +1,3 @@
 library flutter_logger;
 
 export 'src/logger_src.dart';
-
-/*
-
-import 'dart:async';
-
-import 'package:flutter/services.dart';
-
-class FlutterLogs {
-  static const MethodChannel _channel =
-      const MethodChannel('flutter_logs');
-
-  static Future<String?> get platformVersion async {
-    final String? version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
-  }
-}
-*/
--- lib/src/logger_src.dart
+++ lib/src/logger_src.dart
@@ -2,9 +2,12 @@
 ///
 /// Use with EmojiLogger.d('what ever...')
 class EmojiLogger {
+  /// this function will add a smiley face emoji to your log message
   static d(String message){
     print('🤣 $message');
   }
+
+  /// this function will add a bug emoji to your log message
   static e(String message){
     print('👾 $message');
   }
