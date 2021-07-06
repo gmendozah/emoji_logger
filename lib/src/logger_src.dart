ce1cc5df5053beaf54504ca3c7f8f412111fecff
gmendozah <geovanimendoza.h@gmail.com>
2021-07-05 22:34:40 -0400

    Initial commit

---
+++ lib/src/logger_src.dart
@@ -0,0 +1,11 @@
+/// Print logs using emoji
+///
+/// Use with EmojiLogger.d('what ever...')
+class EmojiLogger {
+  static d(String message){
+    print('🤣 $message');
+  }
+  static e(String message){
+    print('👾 $message');
+  }
+}
